b2dac3da4f07825fb3d05e2296429a85220983ff
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-05-29 15:32:14 +0200

    creato l'Html

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
+import { useState } from "react"
+// CREATO L'ARRAY CHE PARTE DA STRINGHE VUOTE //
+const formInitial = {
+  author:"",
+  title:"",
+  body:"",
+}
+
 export  default function App(){
+  const [formData, setFormData] = useState(formInitial);
+
+  // FUNZIONE CHE CAMBIA INPUT //
+  const handleChangeInput = (e) =>{
+    setFormData({...formData,[e.target.name]: e.target.value})
+  }
+
   return (
     <>
       <div className="container">
         <h1>My React</h1>
         <form className="row">
-          <div className="col-3">
+          <div className="col-4">
             <label htmlFor="">Author</label>
-            <input className="form-control" type="text" name="author" onChange={()=>{}} value={""} id="name"></input>
+            <input className="form-control" 
+            type="text" 
+            name="author" 
+            onChange={handleChangeInput} 
+            value={formData.author} 
+            id="name"></input>
           </div>
-          <div className="col-3">
+          <div className="col-4">
             <label htmlFor="">Title</label>
-            <input className="form-control" type="text" name="title" onChange={()=>{}} value={""} id="title"></input>
+            <input className="form-control" 
+            type="text" 
+            name="title" 
+            onChange={handleChangeInput} 
+            value={formData.title} 
+            id="title"></input>
           </div>
-          <div className="col-3">
+          <div className="col-4">
             <label htmlFor="">Body</label>
-            <input className="form-control" type="text" name="body" onChange={()=>{}} value={""} id="body"></input>
+            <input className="form-control" 
+            type="text" 
+            name="body" 
+            onChange={handleChangeInput} 
+            value={formData.body} 
+            id="body"></input>
           </div>
-          <div className="col-3">
-            <label htmlFor="">Public</label>
-            <input className="form-control" type="text" name="public" onChange={()=>{}} value={""} id="public"></input>
+          <div className="col-12">
+            <button className="btn btn-primary">Invio</button>
           </div>
         </form>
       </div>
